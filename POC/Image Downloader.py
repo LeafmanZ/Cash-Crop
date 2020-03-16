c47aae9d5c622dbc701605743238fee3c1f552ed
Jim Zieleman <zielemanj@gmail.com>
2020-03-15 23:43:19 -0400

    Add files via upload

---
+++ POC/Image Downloader.py
@@ -0,0 +1,33 @@
+# Import modules
+import pandas as pd
+import urllib.request
+
+
+def url_to_jpg(i, url, file_path):
+    '''
+        Args:
+            -- i : number of image.
+            -- url : a URL address of a given image.
+            -- file_path : where to save the final image.
+    '''
+
+    filename = 'image-{}.jpg'.format(i)
+    full_path = '{}{}'.format(file_path, filename)
+    urllib.request.urlretrieve(url, full_path)
+
+    print('{} saved.'.format(filename))
+
+    return None
+
+
+# --------------------------------------------
+
+# Set Constants
+FILENAME = 'image csv/inaturalist nightshade woody.csv'
+FILE_PATH = 'images/Nightshade Woody/Nightshade Woody'
+
+urls = pd.read_csv(FILENAME)
+
+# Save Images to the Directory by iterating through the list
+for i, url in enumerate(urls.values):
+    url_to_jpg(i, url[0], FILE_PATH)
